acbaaa7e8c41382ed960477945fda7c7f90b00b7
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-03-17 01:16:35 +0100

    Implemented accepting list invitation

--- share.py
+++ share.py
@@ -8,12 +8,15 @@ from datetime import datetime, timedelta
 from utils.decimalencoder import DecimalEncoder
 import base64
 
+from lists import add_or_update
+
 
 dynamodb = boto3.resource('dynamodb')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)  # TODO: parametrize
 
 l_table = dynamodb.Table(os.environ['DYNAMODB_MAIN_TABLE'])
+utl_table = dynamodb.Table(os.environ['DYNAMODB_USER_TO_LISTS_TABLE'])
 sharing_table = dynamodb.Table(os.environ['DYNAMODB_LISTS_SHARE_TABLE'])
 
 
@@ -23,7 +26,7 @@ def share_list(event, context):
         body = base64.b64decode(event['body'])
     else:
         body = event['body']
-    
+
     try:
         seconds_from_now = int(body)
     except ValueError:
@@ -69,4 +72,72 @@ def share_list(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps(item, cls=DecimalEncoder)
+    }
+
+
+def accept_invitation(event, context):
+    user_data = event['requestContext']['authorizer']['jwt']['claims']
+
+    invitation_code = event['pathParameters']['code']
+    invitation = sharing_table.get_item(
+        Key={
+            'id': invitation_code
+        }
+    )
+
+    if 'Item' not in invitation:
+        return {
+            'statusCode': 401,
+            'body': json.dumps({
+                'status': 'error',
+                'reason': 'Wrong link or invitation has expired'
+            })
+        }
+
+    list_id = invitation['Item']['list_id']
+
+    res = l_table.get_item(
+        Key={
+            'id': list_id
+        }
+    )
+    if 'Item' not in res:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                'status': 'error',
+                'reason': 'The list has been deleted'
+            })
+        }
+
+    if res['Item']['user_id'] == user_data['sub'] or user_data['sub'] in res['Item']['guests']:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'status': 'error',
+                'reason': 'Access to this list has already been granted'
+            })
+        }
+
+    l_table.update_item(
+        Key={
+            'id': list_id
+        },
+        UpdateExpression="SET guests = list_append(guests, :g)",
+        ExpressionAttributeValues={
+            ':g': [user_data['sub']]
+        }
+    )
+
+    add_or_update(user_data['sub'], list_id)
+
+    res = l_table.get_item(
+        Key={
+            'id': list_id
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(res['Item'], cls=DecimalEncoder)
     }
