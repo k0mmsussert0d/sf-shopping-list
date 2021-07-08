41a49ba40378d2b6acca31f34afbb0785102e421
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-07-09 00:25:57 +0200

    refactor: Moved API to separate stack

---
+++ frontend/src/config.js
@@ -0,0 +1,13 @@
+const config = {
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://2wcthjkz2i.execute-api.us-east-1.amazonaws.com',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_YgAizQBax',
+    APP_CLIENT_ID: '998100sg0kq19j46r0g485iar',
+  }
+};
+
+export default config;
